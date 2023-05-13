2f998a2b0c77dd95134f477fd8cd8b7ce3718f91
SilveerDusk <114632851+SilveerDusk@users.noreply.github.com>
2023-05-13 10:46:21 -0700

    Created Abstract Hooks
    Created abstract functions to track the current guess, the turns number, past guesses, set screen after guesses, formatting guesses, and keyups.

---
+++ src/hooks/useWorldle.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+const useWordle = (solution) => {
+    const [turn, setTurn] = useState(0)
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([])
+    const [history, setHistory] = useState([])
+    const [isCorrect, setIsCorrect] = useState(false)
+
+    //format a new guess into an array of letter objects
+    //[{key: 'a'}, {color: 'green'}]
+    const formatGuess = () => {
+
+    }
+
+    //add new guess to screen
+    //update the isCorrect state if guess is correct
+    //add one to turn
+    const addNewGuess = () => {
+
+    }
+
+    //handle keyup event and track current guess
+    //when user presses enter, the guess should be added
+    const handleKeyup = () => {
+
+    }
+
+    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+
+}
+
+export default useWordle
